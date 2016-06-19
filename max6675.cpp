36c79f2e26be1206df2e05e8c723907eae7dd89c
Uli <githubcommits@wolf-u.li>
2016-06-19 13:29:47 +0200

    Forgot the max6675.cpp

--- max6675.cpp
+++ max6675.cpp
@@ -81,14 +81,14 @@ byte MAX6675::spiread(void) {
   for (i=7; i>=0; i--)
   {
     digitalWrite(sclk, LOW);
-    _delay_ms(1);
+    delayMicroseconds(1);
     if (digitalRead(miso)) {
       //set the bit to 0 no matter what
       d |= (1 << i);
     }
 
     digitalWrite(sclk, HIGH);
-    _delay_ms(1);
+    delayMicroseconds(1);
   }
 
   return d;
