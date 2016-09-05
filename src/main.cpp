8770b92d13b1465a2964fb15646ca356e9f67aa7
Eric Marcoux <erifsx@gmail.com>
2016-09-05 11:16:35 -0400

    Adds temperature reading from MAX6675 dumped to serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,26 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <max6675.h>
 
+/**
+ * Thermocouple ports
+ * D6 => SO (MISO)
+ * D5 => SCK
+ * D8 => CS
+ */
+MAX6675 thermocouple;
 
 void setup(){
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
+  SPI.begin();
+  thermocouple.begin(D8);
+  delay(500); // waiting for MAX6675 chip to stabalize
 }
 
 void loop() {
+  const double temp = thermocouple.readFahrenheit();
   delay(1000);
-  Serial.printf("It works\n");
+  Serial.printf("Temp = ");
+  Serial.println(temp);
 }
